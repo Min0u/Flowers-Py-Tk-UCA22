3e9bb94ada7db009f255849a9b91d8980ce4e40c
_Mi.n0u_ <105317987+Min0u@users.noreply.github.com>
2022-05-12 01:09:27 +0200

    Update Fleurs avec une durée de vie.py

--- Fleurs avec une durée de vie.py
+++ Fleurs avec une durée de vie.py
@@ -23,7 +23,7 @@ def clic(event):
     nvlFleur.dessiner()
     fleurs.append(nvlFleur)
 
-# En cliquant s'exécute clic
+# En cliquant clic s'exécute
 surface_dessin.bind('<Button-1>', clic)
 surface_dessin.pack(padx =5, pady =5)
 
